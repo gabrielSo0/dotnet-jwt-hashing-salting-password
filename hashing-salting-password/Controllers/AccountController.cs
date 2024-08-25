1313c8533fcd99f21558b5efb138060baa50c1ba
Gabriel <gabrielsoliveira043@gmail.com>
2024-08-25 03:39:48 -0300

    Adding the first model and controller

---
+++ hashing-salting-password/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hashing_salting_password.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/v1")]
+    public class AccountController : ControllerBase
+    {
+        
+    }
+}
